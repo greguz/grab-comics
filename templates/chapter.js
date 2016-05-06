8773860205b5c56a624da275f46961d53a963465
greguz <gregolettogiacomo@gmail.com>
2016-05-06 17:25:18 +0200

    removed compiled templates

--- templates/chapter.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(Handlebars) {
-
-return Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "<div id=\"pagination\"></div>\n\n<div class=\"container\" id=\"chapterPage\" style=\"margin-bottom: 15px;\"></div>";
-},"useData":true});
-
-};

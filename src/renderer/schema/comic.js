2661849292eda7609d39946a79f8379b0039ae4e
greguz <gregolettogiacomo@gmail.com>
2019-01-30 14:00:17 +0100

    Add schema dir

---
+++ src/renderer/schema/comic.js
@@ -0,0 +1,16 @@
+export default {
+  type: "object",
+  properties: {
+    id: {
+      type: "string"
+    },
+    title: {
+      type: "string"
+    },
+    thumbnail: {
+      type: "string"
+      // TODO: may be undefined
+    }
+    // TODO: meta
+  }
+};

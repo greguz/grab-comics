7e4a6b30f9a3290f571f20dafb8e2a0dfeb916a8
greguz <gregolettogiacomo@gmail.com>
2019-01-31 18:50:48 +0100

    Improve plugin code

--- src/renderer/plugin/comics.js
+++ src/renderer/plugin/comics.js
@@ -1,6 +1,11 @@
-import { ctxToEnv, run, tplToCmd } from "./helpers";
-import comicSchema from "../schema/comic";
+import spawn from "cross-spawn";
+import JSONStream from "JSONStream";
+import { pipeline } from "stream";
 import levenshtein from "js-levenshtein";
+
+import comicSchema from "../schema/comic";
+
+import { buildStringMatcher, ctxToEnv, filter, map, tplToCmd } from "./helpers";
 
 function addDistance(comic, text) {
   return {
@@ -9,14 +14,33 @@ function addDistance(comic, text) {
   };
 }
 
-export default function comics(plugins, text, onData) {
-  const env = ctxToEnv({ text });
+function run(command, language, text, onData) {
+  const match = buildStringMatcher(text);
 
-  const _onData = comic => onData(addDistance(comic, text));
+  return new Promise((resolve, reject) => {
+    pipeline(
+      // Spawn configured command
+      spawn(command[0], command.splice(1)).stdout,
+      // Parse stdout as JSON
+      JSONStream.parse("*"),
+      // Get only valid comics
+      matchSchema(comicSchema),
+      // Filter by searched text and selected language
+      filter(comic => comic.language === language && match(comic.title)),
+      // Add levenshtein distance to comic object
+      map(comic => addDistance(comic, text)),
+      // Final callback
+      err => (err ? reject(err) : resolve())
+    ).on("data", onData);
+  });
+}
+
+export default function comics(plugins, language, text, onData) {
+  const env = ctxToEnv({ language, text });
 
   return Promise.all(
     plugins
       .map(plugin => tplToCmd(plugin.commands.comics, env))
-      .map(command => run(command, comicSchema, _onData))
+      .map(command => run(command, language, text, onData))
   );
 }
--- src/renderer/plugin/helpers.js
+++ src/renderer/plugin/helpers.js
@@ -1,9 +1,25 @@
-import spawn from "cross-spawn";
-import JSONStream from "JSONStream";
-import { Transform, pipeline } from "stream";
 import Ajv from "ajv";
 import shell from "shell-quote";
+import { Transform } from "stream";
 import traverse from "traverse";
+import escapeRegExp from "lodash/escapeRegExp";
+import uniq from "lodash/uniq";
+
+export function buildStringMatcher(search) {
+  const terms = search.match(/\S+/g) || [];
+  if (terms.length <= 0) {
+    return () => false;
+  }
+  const exps = uniq(terms).map(term => new RegExp(escapeRegExp(term), "i"));
+  return function match(title) {
+    for (const exp of exps) {
+      if (!exp.test(title)) {
+        return false;
+      }
+      return true;
+    }
+  };
+}
 
 export function ctxToEnv(ctx) {
   const env = {};
@@ -30,38 +46,62 @@ export function tplToCmd(tpl, env) {
   return cmd;
 }
 
-function getSchemaError(errors, data, schema) {
-  const err = new Error("Found invalid JSON object");
-  err.data = data;
-  err.schema = schema;
-  err.errors = errors;
-  return err;
-}
-
-function matchSchema(schema) {
-  const ajv = new Ajv();
-  const validate = ajv.compile(schema);
-
+export function mapAsync(fn) {
   return new Transform({
     objectMode: true,
     transform(data, encoding, callback) {
-      if (validate(data) === true) {
-        this.push(data);
-        callback();
-      } else {
-        callback(getSchemaError(validate.errors, data, schema));
-      }
+      fn(data, (err, result) => {
+        if (
+          (err === undefined || err === null) &&
+          result !== undefined &&
+          result !== null
+        ) {
+          this.push(result);
+        }
+        callback(err);
+      });
     }
   });
 }
 
-export function run(command, schema, onData) {
-  return new Promise((resolve, reject) => {
-    pipeline(
-      spawn(command[0], command.splice(1)).stdout,
-      JSONStream.parse("*"),
-      matchSchema(schema),
-      err => (err ? reject(err) : resolve())
-    ).on("data", onData);
+export function map(fn) {
+  return mapAsync((data, callback) => {
+    callback(null, fn(data));
+  });
+}
+
+export function filterAsync(fn) {
+  return new Transform({
+    objectMode: true,
+    transform(data, encoding, callback) {
+      fn(data, (err, result) => {
+        if ((err === undefined || err === null) && result === true) {
+          this.push(data);
+        }
+        callback(err);
+      });
+    }
+  });
+}
+
+export function filter(fn) {
+  return filterAsync((data, callback) => {
+    callback(null, fn(data));
+  });
+}
+
+export function matchSchema(schema) {
+  const ajv = new Ajv();
+  const validate = ajv.compile(schema);
+
+  return filter(data => {
+    const valid = validate(data);
+
+    if (!valid) {
+      console.error(data);
+      console.error(validate.errors);
+    }
+
+    return valid;
   });
 }

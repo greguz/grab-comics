15c8a1b3b48e37c8f0bab8934622a5fac354dd64
greguz <gregolettogiacomo@gmail.com>
2019-02-02 18:24:31 +0100

    Update chapter schema

--- src/renderer/schema/chapter.js
+++ src/renderer/schema/chapter.js
@@ -1,1 +1,19 @@
-export default {};
+export default {
+  type: "object",
+  required: ["id", "number"],
+  properties: {
+    id: {
+      type: "string"
+    },
+    number: {
+      type: "number",
+      minimum: 1
+    },
+    title: {
+      type: "string"
+    },
+    meta: {
+      // type: "any"
+    }
+  }
+};

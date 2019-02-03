50c23933e5378b65722f14cbca9231ab544849ca
greguz <gregolettogiacomo@gmail.com>
2019-02-03 10:35:18 +0100

    Add basic chapters UI

--- src/renderer/components/Chapters.vue
+++ src/renderer/components/Chapters.vue
@@ -1,11 +1,28 @@
 <template>
-  <h1>TODO</h1>
+  <div>
+    <h1>{ comic.title }</h1>
+    <table>
+      <tbody>
+        <tr v-for="chapter in chapters" v-bind:key="chapter.number">
+          <td>{ chapter.title }</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
+import { mapState } from "vuex";
+
 export default {
+  computed: mapState({
+    comic: "comic",
+    chapters: "chapters"
+  }),
   mounted() {
-    this.$store.dispatch("fetchChapters");
+    if (this.chapters.length <= 0) {
+      this.$store.dispatch("fetchChapters");
+    }
   }
 };
 </script>

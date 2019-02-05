691e5878cfbe38be82dcab0ba1434408ad1f68db
greguz <gregolettogiacomo@gmail.com>
2019-02-05 10:35:30 +0100

    Add basic styles to comics search

--- src/renderer/components/Comics.vue
+++ src/renderer/components/Comics.vue
@@ -2,15 +2,20 @@
   <div class="container">
     <input type="text" id="search" placeholder="Search" v-model="text" v-on:change="search">
     <hr>
-    <img
-      v-for="comic in comics"
-      v-bind:key="comic.id"
-      v-bind:title="comic.title"
-      v-bind:src="comic.thumbnail"
-      v-on:click="selectComic(comic)"
-      height="300"
-      width="200"
-    >
+
+    <div style="display: flex; flex-wrap: wrap; justify-content: space-evenly;">
+      <div v-for="comic in comics" v-bind:key="comic.id">
+        <img
+          v-bind:title="comic.title"
+          v-bind:src="comic.thumbnail"
+          v-on:click="selectComic(comic)"
+          v-on:error="setFallbackThumbnail(comic)"
+          height="280"
+          width="200"
+          style="object-fit: cover;"
+        >
+      </div>
+    </div>
   </div>
 </template>
 
@@ -30,6 +35,10 @@ export default {
     },
     selectComic(comic) {
       this.$store.dispatch("selectComic", comic);
+    },
+    setFallbackThumbnail(comic) {
+      comic.thumbnail =
+        "https://res.cloudinary.com/teepublic/image/private/s--Fq3Bvjo5--/t_Preview/b_rgb:6e2229,c_limit,f_jpg,h_630,q_90,w_630/v1494751362/production/designs/1602004_1.jpg";
     }
   }
 };

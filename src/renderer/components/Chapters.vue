de1728af96afc04178ad8f6419021f262b10ae3c
greguz <gregolettogiacomo@gmail.com>
2019-02-26 11:01:05 +0100

    Improve chapters view

--- src/renderer/components/Chapters.vue
+++ src/renderer/components/Chapters.vue
@@ -1,23 +1,40 @@
 <template>
-  <div>
-    <h1>{{ comic.title }}</h1>
+  <div class="container">
+    <h1 class="title is-1">{{ comic.title }}</h1>
 
-    <table>
-      <tbody>
-        <tr
-          v-for="chapter in chapters"
-          v-bind:key="chapter.number"
-          v-on:click="openChapter(chapter)"
-        >
-          <td>{{ chapter.title }}</td>
-        </tr>
-        <tr v-if="moreAvailable">
-          <td>
-            <button v-on:click="showMoreChapters">More</button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <div class="tile is-ancestor">
+      <div class="tile is-parent">
+        <div class="tile is-6 is-child box">
+          <table class="table is-striped is-narrow is-hoverable is-fullwidth">
+            <thead>
+              <tr>
+                <td width="50" class="has-text-right">#</td>
+                <td>Title</td>
+              </tr>
+            </thead>
+            <tbody>
+              <tr
+                v-for="chapter in chapters"
+                v-bind:key="chapter.number"
+                v-on:click="openChapter(chapter)"
+              >
+                <td class="has-text-right">{{ chapter.number }}</td>
+                <td>{{ chapter.title }}</td>
+              </tr>
+              <tr v-if="moreAvailable">
+                <td></td>
+                <td>
+                  <button v-on:click="showMoreChapters">More</button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div class="tile is-6 is-child box">
+          <p class="title">Two</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 

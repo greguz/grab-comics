7cd051561fb5ae44fc3a778d07c8846047706534
greguz <gregolettogiacomo@gmail.com>
2019-02-26 09:39:47 +0100

    Add assets processing to webpack

--- webpack.renderer.js
+++ webpack.renderer.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const VueLoaderPlugin = require("vue-loader/lib/plugin");
 const { dependencies } = require("./package.json");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   target: "electron-renderer",
@@ -33,6 +34,19 @@ module.exports = {
       }
     ]
   },
-  plugins: [new VueLoaderPlugin()],
+  plugins: [
+    new VueLoaderPlugin(),
+    new CopyPlugin([
+      {
+        from: path.join(__dirname, "src/index.html"),
+        to: path.join(__dirname, "dist/index.html")
+      },
+      {
+        from: path.join(__dirname, "src/assets"),
+        to: path.join(__dirname, "dist/assets"),
+        ignore: ".gitkeep"
+      }
+    ])
+  ],
   externals: Object.keys(dependencies)
 };
